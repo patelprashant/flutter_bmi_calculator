e18700cf2db7dc1156e33886ccbc03c07694a5c1
Prashant <prashant.patel77@gmail.com>
2019-05-30 21:12:32 +1000

    Initialise project

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UserInputPage(title: 'BMI CALCULATOR'),
+    );
+  }
+}
+
+class UserInputPage extends StatefulWidget {
+  UserInputPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _UserInputPageState createState() => _UserInputPageState();
+}
+
+class _UserInputPageState extends State<UserInputPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
