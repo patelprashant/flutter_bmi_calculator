86ed6c790872dc497d5235a255338324140075c5
Prashant <prashant.patel77@gmail.com>
2019-06-02 09:53:35 +1000

    Extracted color to const

--- lib/user_input_page.dart
+++ lib/user_input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardcolor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class UserInputPage extends StatefulWidget {
   UserInputPage({Key key, this.title}) : super(key: key);
   final String title;
@@ -21,27 +25,33 @@ class _UserInputPageState extends State<UserInputPage> {
               child: Row(
             children: <Widget>[
               Expanded(
-                child: ReusableCard(bgColor: Color(0xFF1D1E33)),
+                child: ReusableCard(bgColor: activeCardcolor),
               ),
               Expanded(
-                child: ReusableCard(bgColor: Color(0xFF1D1E33)),
+                child: ReusableCard(bgColor: activeCardcolor),
               ),
             ],
           )),
           Expanded(
-            child: ReusableCard(bgColor: Color(0xFF1D1E33)),
+            child: ReusableCard(bgColor: activeCardcolor),
           ),
           Expanded(
               child: Row(
             children: <Widget>[
               Expanded(
-                child: ReusableCard(bgColor: Color(0xFF1D1E33)),
+                child: ReusableCard(bgColor: activeCardcolor),
               ),
               Expanded(
-                child: ReusableCard(bgColor: Color(0xFF1D1E33)),
+                child: ReusableCard(bgColor: activeCardcolor),
               ),
             ],
           )),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
         ],
       ),
     );
